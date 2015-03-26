cf4a31a085790767eea8ddbd3e0b28075bd47faf
nkoech <koechnicholas@gmail.com>
2015-03-27 00:22:34 +0300

    Added GeoJSON layers

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /**
- * Created by nkoech on 28/02/2015.
+ * Created by nkoech on 26/03/2015.
  */
 
 var app = (function(){
@@ -8,106 +8,38 @@ var app = (function(){
         initMap: function() {
 
             //Get layer  token
-            L.mapbox.accessToken = 'pk.eyJ1IjoiYWZyaWNhY3NhIiwiYSI6IjVqZGs5UW8ifQ.i8gT2b0XHC66u7WC_srvRw';
+            L.mapbox.accessToken = 'pk.eyJ1IjoibmtvZWNoIiwiYSI6IllzVF8ya2sifQ.D5WtsqKGS6ulGVPrBwV9mA';
 
             //Set map options
             map = L.mapbox.map('map', null, {
-                maxZoom: 9,
-                minZoom: 3
-            }).setView([1.8047,25.000], 3);
+                maxZoom: 19,
+                minZoom: 9
+            }).setView([-1.2139,37.5320], 9);
         },
 
         getMapComponent: function() {
 
             //Set layers
             layers = {
-                admin0: L.mapbox.tileLayer('africacsa.8451ad0f'),
-                admin1: L.mapbox.tileLayer('africacsa.53abf605'),
-                admin2: L.mapbox.tileLayer('africacsa.5893b9a7')
-            };
-            grids = {
-                admin0: L.mapbox.gridLayer('africacsa.8451ad0f'),
-                admin1: L.mapbox.gridLayer('africacsa.53abf605'),
-                admin2: L.mapbox.gridLayer('africacsa.5893b9a7')
-            };
-            gridControls = {
-                admin0: L.mapbox.gridControl(grids.admin0),
-                admin1: L.mapbox.gridControl(grids.admin1),
-                admin2: L.mapbox.gridControl(grids.admin2)
+                baselayer0: L.mapbox.tileLayer('nkoech.c858c345'),
+                m_jericanused: L.geoJson(m_jericanused),
+                m_storetank: L.geoJson(m_storetank),
+                m_watercost: L.geoJson(m_watercost),
+                m_waterdist: L.geoJson(m_waterdist),
+                m_watertype: L.geoJson(m_watertype)
             };
         },
 
         mapComponent: function() {
-            var legendAdmin0 = "admin0-legend";
-            var legendAdmin2 = "admin2-legend";
-            var legendAdmin1 = "admin1-legend";
 
             //Add initial layer, grid and controls to map
-            layers.admin0.addTo(map);
-            grids.admin0.addTo(map);
-            gridControls.admin0.addTo(map);
-            map.legendControl.addLegend(document.getElementById('admin0-legend').innerHTML);
+            layers.baselayer0.addTo(map);
+            //layers.m_jericanused.addTo(map);
+            //layers.m_storetank.addTo(map);
+            //layers.m_watercost.addTo(map);
+            //layers.m_waterdist.addTo(map);
+            //layers.m_watertype.addTo(map);
 
-            //Change layer and legend on certain zoom levels
-            map.on('zoomend', function() {
-                if(map.getZoom() >= 5 && map.getZoom() <= 7){
-                    if (map.hasLayer(layers.admin0)) {
-                        //Remove admin0 layers and controls
-                        households.removeMapComponent(legendAdmin0,layers.admin0,gridControls.admin0,grids.admin0);
-                        //Add admin1 layers and controls
-
-
-
-                        /*grids.admin1.on('click', function (e) {
-                            if (e.data) {
-                                //gridControls.admin1.setTemplate('<h1>Temperature:</h1>');
-                                console.log(gridControls.admin1);
-                            } else {
-                                //console.log(e.data);
-                            }
-                        });*/
-
-
-
-                        households.addMapComponent(layers.admin1,legendAdmin1,grids.admin1,gridControls.admin1);
-                    }
-                    if (map.hasLayer(layers.admin2)) {
-                        //Remove admin2 layers and controls
-                        households.removeMapComponent(legendAdmin2,layers.admin2,gridControls.admin2,grids.admin2);
-                        //Add admin1 layers and controls
-                        households.addMapComponent(layers.admin1,legendAdmin1,grids.admin1,gridControls.admin1);
-                    }
-                } else if (map.getZoom() >= 8){
-                    if (map.hasLayer(layers.admin1)) {
-                        //Remove admin1 layers and controls
-                        households.removeMapComponent(legendAdmin1,layers.admin1,gridControls.admin1,grids.admin1);
-                        //Add admin2 layers and controls
-                        households.addMapComponent(layers.admin2,legendAdmin2,grids.admin2,gridControls.admin2);
-                    }
-                }else{
-                    if (map.hasLayer(layers.admin1)) {
-                        //Remove admin1 layers and controls
-                        households.removeMapComponent(legendAdmin1,layers.admin1,gridControls.admin1,grids.admin1);
-                        //Add admin0 layers and controls
-                        households.addMapComponent(layers.admin0,legendAdmin0,grids.admin0,gridControls.admin0);
-                    }
-                }
-            });
-        },
-
-        removeMapComponent: function(legend,layer,gridControl,grid){
-
-            map.legendControl.removeLegend(document.getElementById(legend).innerHTML);
-            map.removeLayer(layer);
-            map.removeControl(gridControl);
-            map.removeLayer(grid);
-        },
-
-        addMapComponent: function(layer,legend,grid,gridControl){
-            layer.addTo(map);
-            map.legendControl.addLegend(document.getElementById(legend).innerHTML);
-            grid.addTo(map);
-            gridControl.addTo(map);
         }
     };
 
