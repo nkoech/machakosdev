024c306039feaaa5fff9a0e862c0c0675b008f7e
nkoech <koechnicholas@gmail.com>
2015-03-29 03:55:42 +0300

    Layers Control added to viasualize one layer at a time

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var app = (function(){
 
         initMap: function() {
 
-            //Get layer  token
+            //Get data  token
             L.mapbox.accessToken = 'pk.eyJ1IjoibmtvZWNoIiwiYSI6IllzVF8ya2sifQ.D5WtsqKGS6ulGVPrBwV9mA';
 
             //Set map options
@@ -21,25 +21,73 @@ var app = (function(){
 
             //Set layers
             layers = {
-                baselayer0: L.mapbox.tileLayer('nkoech.c858c345'),
+                basemap: L.mapbox.tileLayer('nkoech.c858c345').addTo(map),
                 m_jericanused: L.geoJson(m_jericanused),
                 m_storetank: L.geoJson(m_storetank),
-                m_watercost: L.geoJson(m_watercost),
-                m_waterdist: L.geoJson(m_waterdist),
-                m_watertype: L.geoJson(m_watertype)
+                m_watercost: L.geoJson(m_watercost)
             };
+            /*m_waterdist: L.geoJson(m_waterdist),
+             m_watertype: L.geoJson(m_watertype)*/
         },
 
-        mapComponent: function() {
+       mapComponent: function() {
 
-            //Add initial layer, grid and controls to map
-            layers.baselayer0.addTo(map);
+            //Add initial data, grid and controls to map
+            layers.basemap.addTo(map);
             //layers.m_jericanused.addTo(map);
             //layers.m_storetank.addTo(map);
             //layers.m_watercost.addTo(map);
             //layers.m_waterdist.addTo(map);
             //layers.m_watertype.addTo(map);
 
+            households.addMapComponent(layers.m_jericanused, 'm_jericanused', 'Jerican Used', 1);
+            households.addMapComponent(layers.m_storetank, 'm_storetank', 'Store Tank', 2);
+            households.addMapComponent(layers.m_watercost, 'm_watercost', 'Cost of Water', 3);
+        },
+
+        addMapComponent: function(fLayer, elId, name, zIndex){
+            var layersList = document.getElementById('menu-ui');
+            var link = document.createElement('a');
+
+            if(zIndex == 1){
+                fLayer.setZIndex(zIndex).addTo(map);
+                link.href = '#';
+                link.id = elId;
+                link.className = 'active';
+                link.innerHTML = name;
+            }else{
+                link.href = '#';
+                link.id = elId;
+                link.className = '';
+                link.innerHTML = name;
+            }
+
+            // Create a simple layer switcher that
+            // toggles layers on and off.
+            link.onclick = function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+
+                if(this.className != "active"){
+
+                    elMenuUi = map.getContainer().querySelector(".menu-ui");
+                    elAll = elMenuUi.querySelectorAll(".active");
+
+                    for (i = 0; i < elAll.length; i++) {
+                        map.removeLayer(layers[elAll[i].id]);
+                        //console.log(elAll[i].id);
+                        elAll[i].className = '';
+                    }
+
+                    fLayer.addTo(map);
+                    this.className = 'active';
+                }else{
+                    map.removeLayer(fLayer);
+                    this.className = '';
+                }
+            };
+
+            layersList.appendChild(link);
         }
     };
 
